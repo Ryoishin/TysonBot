2f733ca321600bb2fdb0cf13a2b8eee8be7fa470
Tyson-14 <98878110+Tyson-14@users.noreply.github.com>
2022-02-18 15:39:49 +0530

    Create Tyson.py

---
+++ Tyson.py
@@ -0,0 +1,13 @@
+import logging
+from os import system
+
+# Logs
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
+# Starting bot
+system("git clonehttps://github.com/Tyson-14/TysonBot && cd TysonBot && python3 -m userbot")
+# if got error
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @The_Tysonsupport")
+# Script written for stop heroku suspension
+# use with credit else gay
